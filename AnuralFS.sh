5a20d7e5297a211c5cb9c0d2454ff93fb779c3d1
Dddpwpp <147535933+Dddpwpp@users.noreply.github.com>
2024-04-19 23:39:08 -0400

    Create AnuralFS.sh

---
+++ AnuralFS.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ]; then
+    echo "Run this script as root (sudo su)"
+    exit
+fi
+
+echo "+##############################################+"
+echo "# Welcome To AnuralFS!                         #"
+echo "# The User Policy Editor                       #"
+echo "# -------------------------------------------- #"
+echo "# Developers:                                  #"
+echo "# - Xotic                                      #"
+echo "# - null                                       #"
+echo "# - null                                       #"
+echo "# - null                                       #"
+echo "# - null                                       #"
+echo "+##############################################+"
+echo "May Ultrablue rest in piece, o7"
+
+sleep 1
+
+mkdir -p /etc/opt/chrome/policies/managed
+printf '
+{
+    "URLBlocklist": [], 
+    "URLAllowlist": [],
+    "SystemFeaturesDisableList": [], 
+    "EditBookmarksEnabled": true, 
+    "ChromeOsMultiProfileUserBehavior": "unrestricted", 
+    "DeveloperToolsAvailability": 1,
+    "DefaultPopupsSetting": 1, 
+    "AllowDeletingBrowserHistory": true, 
+    "AllowDinosaurEasterEgg": true, 
+    "DisableSafeBrowsingProceedAnyway": false,
+    "IncognitoModeAvailability": 0, 
+    "AllowScreenLock": true, 
+    "ExtensionAllowedTypes": null, 
+    "ExtensionInstallAllowlist": null, 
+    "ExtensionInstallBlocklist": null, 
+    "ExtensionInstallForcelist": null, 
+    "ExtensionSettings": null, 
+    "ForceGoogleSafeSearch": false,
+    "ForceYouTubeRestrict": 0,
+    "DeviceAllowNewUsers": true,
+    "DeviceGuestModeEnabled": true,
+    "DeveloperToolsDisabled": false,
+    "DeviceBlockDevmode": false,
+    "DevicePowerwashAllowed": true,
+    "LacrosAvailability": "user_choice", 
+    "InstantTetheringAllowed": true,
+    "SafeSitesFilterBehavior": 0,
+    "SafeBrowsingProtectionLevel": 0,
+    "DownloadRestrictions": 0,
+    "ProxyMode": "system",
+    "ProxyServerMode": "system",
+    "WallpaperImage": null
+}
+' > /etc/opt/chrome/policies/managed/pollen.json
+
+echo ""
+echo "AnuralFS has been successfully applied!"
