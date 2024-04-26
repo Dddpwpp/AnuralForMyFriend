baf86033fa5177f5cde41930fc5176183e84815f
Dddpwpp <147535933+Dddpwpp@users.noreply.github.com>
2024-04-26 11:10:29 -0400

    Update anural.sh

--- anural.sh
+++ anural.sh
@@ -6,25 +6,107 @@ if [ $(id -u) -ne 0 ]; then
 fi
 
 echo "+##############################################+"
-echo "# Welcome To Anural!                           #"
+echo "# Welcome To Pollen!                           #"
 echo "# The User Policy Editor                       #"
 echo "# -------------------------------------------- #"
 echo "# Developers:                                  #"
-echo "# - Xotic - Lead developer, added the policies #"
-echo "# - OlyB - Did some of the coding              #"
-echo "# - Rafflesia - Fixing errors                  #"
-echo "# - Scaratek - Coming up with this insane idea #"
-echo "# - r58Playz - Additional Changes              #"
+echo "# - OlyB                                       #"
+echo "# - Rafflesia                                  #"
+echo "# - Scaratek                                   #"
+echo "# - r58Playz                                   #"
 echo "# - Honkychunkymonkey - Additional Changes     #"
 echo "+##############################################+"
 echo "May Ultrablue Rest in Peace, o7"
 
 sleep 1
 
+# Check if the policy.json already exists and back it up as old.policies.json
+POLICY_FILE="/etc/opt/chrome/policies/managed/policy.json"
+BACKUP_DIR="/home/chronos/user/Downloads"
+if [ -f "$POLICY_FILE" ]; then
+    echo "Backing up existing policy.json to $BACKUP_DIR/old.policies.json"
+    cp "$POLICY_FILE" "$BACKUP_DIR/old.policies.json"
+fi
+
 mkdir -p /tmp/overlay/etc/opt/chrome/policies/managed
-echo '{"URLBlocklist": [ "chrome://policy" ], "SystemFeaturesDisableList": [], "EditBookmarksEnabled": true, "ChromeOsMultiProfileUserBehavior": "unrestricted", "DeveloperToolsAvailability": 1, "DefaultPopupsSetting": 1, "AllowDeletingBrowserHistory": true, "AllowDinosaurEasterEgg": true, "AllowKioskAppControlChromeVersion": true, "IncognitoModeAvailability": 0, "AllowScreenLock": true, "ArcAppInstallEventLoggingEnabled": false, "ExtensionAllowedTypes": null, "ExtensionInstallAllowlist": null, "ExtensionInstallBlocklist": null, "ExtensionInstallForcelist": kbfnbcaeplbcioakkpcpgfkobkghlhen, cjpalhdlnbpafiamejdnhcphjbkeiagm, "ExtensionSettings": null, "PasswordManagerEnabled": "true", "TaskManagerEndProcessEnabled": "false", "UptimeLimit": "17860", "SystemTerminalSshAllowed": "false", "SystemTimezone": "", "IsolatedAppsDeveloperModeAllowed": "true", "ForceGoogleSafeSearch": "true", "ForceYouTubeRestrict": "0", "EasyUnlockAllowed": "false", "DisableSafeBrowsingProceedAnyway": "false", "DeviceAllowNewUsers": "true", "DevicePowerAdaptiveChargingEnabled": "true", "DeviceGuestModeEnabled": "false", "DeviceUnaffiliatedCrostiniAllowed": "true", "VirtualMachinesAllowed": "true", "CrostiniAllowed": "true", "DefaultCookiesSetting": "1", "VmManagementCliAllowed": "true", "WifiSyncAndroidAllowed": "false", "DeveloperToolsDisabled": "false", "DeveloperToolsAvailability": "1", "DeviceBlockDevmode": "false", "UserBorealisAllowed": "true", "InstantTetheringAllowed": "true", "NearbyShareAllowed": "false", "PinnedLauncherApps": null, "PrintingEnabled": "true", "SmartLockSigninAllowed": "false", "SmsMessagesAllowed": "false", "PhoneHubAllowed": "false", "LacrosAvailability": "user_choice", "LacrosSecondaryProfilesAllowed": "false", "RebootAfterUpdate": "true", "ReportDeviceActivityTimes": "true", "ReportCRDSessions": "true", "ReportDeviceAudioStatus": "true", "ReportDeviceAudioStatusCheckingRateMs": "900000", "ReportDeviceBacklightInfo": "true", "ReportDeviceBluetoothInfo": "true", "ReportDeviceBoardStatus": "true", "ReportDeviceBootMode": "true", "ReportDeviceCpuInfo": "true", "ReportDeviceCrashReportInfo": "true", "ReportDeviceFanInfo": "true", "ReportDeviceGraphicsStatus": "true", "ReportDeviceHardwareStatus": "true", "ReportDeviceLoginLogout": "true", "ReportDeviceMemoryInfo": "true", "ReportDeviceNetworkConfiguration": "true", "ReportDeviceNetworkInterfaces": "true", "ReportDeviceNetworkStatus": "true", "ReportDeviceNetworkTelemetryCollectionRateMs": "true", "ReportDeviceOsUpdateStatus": "true", "ReportDevicePeripherals": "true", "ReportDevicePowerStatus": "true", "ReportDevicePrintJobs": "true", "ReportDeviceSecurityStatus": "true", "ReportDeviceSessionStatus": "true", "ReportDeviceStorageStatus": "false", "ReportDeviceSystemInfo": "true", "ReportDeviceTimezoneInfo": "true", "ReportDeviceUsers": "true", "ReportDeviceVersionInfo": "true", "ReportDeviceVpdInfo": "true", "ReportUploadFrequency" "10800000", "BrowserAddPersonEnabled": "true", "WallpaperImage": null, "DeviceGuestModeALlowed": "false", "RestoreOnStartup": "1", "CACertificateManagementAllowed": "2", "AdsSettingForIntrusiveAdsSites": "1", "SuggestedContentEnabled": "true", "CastReceiverEnabled": "false", "ClientCertificateManagementAllowed": "2", "DeviceDebugPacketCaptureAllowed": "true", "LoginDisplayPasswordButtonEnabled": "false", "TranslateEnabled": "true", "UserFeedbackAllowed": "false", "BookmarkBarEnabled": "true", "DnsOverHttpsMode": "automatic", "BrowserLabsEnabled": "false", "ChromeOsReleaseChannelDelegated": "true", "WallpaperImage": "null", "SafeSitesFilterBehavior": "1", "SafeBrowsingProtectionLevel": "1", "DownloadRestrictions": "4", "ProxyMode": "system", "ProxyServerMode": "system", "NetworkThrottlingEnabled": "false", "NetworkPredictionOptions": "0", "AllowedDomainsForApps": "gmail.com,mpasd.net", "DeviceUserAllowlist": ""}' > /tmp/overlay/etc/opt/chrome/policies/managed/policy.json
+echo '{
+  "URLBlocklist": [],
+  "SystemFeaturesDisableList": [],
+  "EditBookmarksEnabled": true,
+  "ChromeOsMultiProfileUserBehavior": "unrestricted",
+  "DeveloperToolsAvailability": 1,
+  "DefaultPopupsSetting": 1,
+  "AllowDeletingBrowserHistory": true,
+  "AllowDinosaurEasterEgg": true,
+  "IncognitoModeAvailability": 0,
+  "AllowScreenLock": true,
+  "ExtensionAllowedTypes": null,
+  "ExtensionInstallAllowlist": null,
+  "ExtensionInstallBlocklist": null,
+  "ExtensionInstallForcelist": null,
+  "ExtensionSettings": null,
+  "PasswordManagerEnabled": "true",
+  "TaskManagerEndProcessEnabled": "true",
+  "UptimeLimit": "null",
+  "SystemTerminalSshAllowed": "true",
+  "SystemTimezone": "",
+  "IsolatedAppsDeveloperModeAllowed": "true",
+  "ForceGoogleSafeSearch": "false",
+  "ForceYouTubeRestrict": "0",
+  "EasyUnlockAllowed": "true",
+  "DisableSafeBrowsingProceedAnyway": "false",
+  "DeviceAllowNewUsers": "true",
+  "DevicePowerAdaptiveChargingEnabled": "true",
+  "DeviceGuestModeEnabled": "true",
+  "DeviceUnaffiliatedCrostiniAllowed": "true",
+  "VirtualMachinesAllowed": "true",
+  "CrostiniAllowed": "true",
+  "DefaultCookiesSetting": "1",
+  "VmManagementCliAllowed": "true",
+  "WifiSyncAndroidAllowed": "true",
+  "DeveloperToolsDisabled": "false",
+  "DeveloperToolsAvailability": "1",
+  "DeviceBlockDevmode": "false",
+  "UserBorealisAllowed": "true",
+  "InstantTetheringAllowed": "true",
+  "NearbyShareAllowed": "true",
+  "PinnedLauncherApps": "null",
+  "PrintingEnabled": "true",
+  "SmartLockSigninAllowed": "true",
+  "PhoneHubAllowed": "true",
+  "LacrosAvailability": "user_choice",
+  "ArcPolicy": {
+    "playStoreMode": "ENABLED",
+    "installType": "FORCE_INSTALLED",
+    "playEmmApiInstallDisabled": false,
+    "dpsInteractionsDisabled": false
+  },
+  "DnsOverHttpsMode": "automatic",
+  "BrowserLabsEnabled": "true",
+  "ChromeOsReleaseChannelDelegated": "true",
+  "SafeSitesFilterBehavior": "0",
+  "SafeBrowsingProtectionLevel": "0",
+  "DownloadRestrictions": "0",
+  "ProxyMode": "system",
+  "ProxyServerMode": "system",
+  "NetworkThrottlingEnabled": "false",
+  "NetworkPredictionOptions": "0",
+  "AllowedDomainsForApps": "",
+  "DeviceUserAllowlist": "",
+  "BlockThirdPartyCookies": "",
+  "ChromeOsReleaseChannel": "",
+  "FastPairEnabled": "true",
+  "WallpaperImage": {
+      "hash": "null",
+      "url": "null"
+  },
+  "WebAppInstallForceList": "",
+  "LacrosSecondaryProfilesAllowed": "true",
+  "LacrosSelection": "user_choice",
+  "DownloadDirectory": ""
+}' > /tmp/overlay/etc/opt/chrome/policies/managed/policy.json
 cp -a -L /etc/* /tmp/overlay/etc 2> /dev/null
 mount --bind /tmp/overlay/etc /etc
 
 echo ""
-echo "Anural has been successfully applied!"
+echo "Pollen - 0.1.3 has been successfully applied!"
